40f78e440c65c77786ea958013b5a8607a0814c3
hsu-hua-chun <fet502315@gmail.com>
2025-04-25 13:41:26 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base:'/world-time/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  // 輸入npm run dev後會自己打開頁面
+  server:{
+    open:true
+  }
+})
